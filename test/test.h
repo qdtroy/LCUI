570d612315e42f562844cb8278c9c64208ac8ee1
Liu <lc-soft@live.cn>
2019-08-11 13:11:48 +0800

    feat(graph): rounded box shadow (backup 1)

--- test/test.h
+++ test/test.h
@@ -1,36 +1,38 @@
-﻿
+﻿#include <stdio.h>
+
 extern int tests_count;
 
 #define TEST_LOG(format, ...) \
-	Logger_Log("[test] %s(): " format, __FUNCTION__, ##__VA_ARGS__)
+	printf("[test] %s(): " format, __FUNCTION__, ##__VA_ARGS__)
 
 #define PRINT_TEST_RESULT(N) \
-	Logger_Log("[test] %d tests, %d pass.\n", tests_count, tests_count + N)
+	printf("[test] %d tests, %d pass.\n", tests_count, tests_count + N)
 
-#define CHECK(X)                                                       \
-	do {                                                           \
-		tests_count += 1;                                      \
-		LOG("[test] %s(): %s. # %s\n", __FUNCTION__, "" #X "", \
-		    (X) ? "PASS" : (ret -= 1, "NO PASS!"));            \
+#define CHECK(X)                                                          \
+	do {                                                              \
+		tests_count += 1;                                         \
+		printf("[test] %s(): %s. # %s\n", __FUNCTION__, "" #X "", \
+		       (X) ? "PASS" : (ret -= 1, "NO PASS!"));            \
 	} while (0);
 
-#define CHECK_WITH_TEXT(TEXT, X)                                   \
-	do {                                                       \
-		tests_count += 1;                                  \
-		LOG("[test] %s(): %s. # %s\n", __FUNCTION__, TEXT, \
-		    (X) ? "PASS" : (ret -= 1, "NO PASS!"));        \
+#define CHECK_WITH_TEXT(TEXT, X)                                      \
+	do {                                                          \
+		tests_count += 1;                                     \
+		printf("[test] %s(): %s. # %s\n", __FUNCTION__, TEXT, \
+		       (X) ? "PASS" : (ret -= 1, "NO PASS!"));        \
 	} while (0);
 
-#define CHECK2(X)                                                          \
-	do {                                                               \
-		if (!(X)) {                                                \
-			LOG("[test] %s(): %s. # NO PASS!\n", __FUNCTION__, \
-			    "" #X "");                                     \
-			ret -= 1;                                          \
-		}                                                          \
+#define CHECK2(X)                                                             \
+	do {                                                                  \
+		if (!(X)) {                                                   \
+			printf("[test] %s(): %s. # NO PASS!\n", __FUNCTION__, \
+			       "" #X "");                                     \
+			ret -= 1;                                             \
+		}                                                             \
 	} while (0);
 
 int test_border(void);
+int test_box_shadow(void);
 int test_charset(void);
 int test_string(void);
 int test_object(void);
